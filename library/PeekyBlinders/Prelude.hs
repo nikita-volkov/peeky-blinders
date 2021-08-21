b571e21ad1f45b737887176327f00696ab372415
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-21 11:15:21 +0300

    Add Text to Prelude

--- library/PeekyBlinders/Prelude.hs
+++ library/PeekyBlinders/Prelude.hs
@@ -44,6 +44,7 @@ import Data.Ratio as Exports
 import Data.STRef as Exports
 import Data.Semigroup as Exports hiding (First (..), Last (..))
 import Data.String as Exports
+import Data.Text as Exports (Text)
 import Data.Traversable as Exports
 import Data.Tuple as Exports
 import Data.Unique as Exports
