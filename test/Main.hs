b49b7b65385778c304fd1eb7d1c6150fa447ced7
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-23 11:33:03 +0300

    Test out the C-string stuff

--- test/Main.hs
+++ test/Main.hs
@@ -13,5 +13,15 @@ import Prelude hiding (all, choose)
 main = defaultMain $ testGroup "All" all
 
 all =
-  [
+  [ testCase "Unterminated C-string" $ do
+      assertEqual "" Nothing $
+        either (const Nothing) Just $
+          Pb.decodeByteString Pb.byteStringTerminatedByNull "\1\2\3\4",
+    testCase "Terminated C-string" $ do
+      assertEqual "" (Right "abc") $
+        Pb.decodeByteString Pb.byteStringTerminatedByNull "abc\0d",
+    testCase "Composition after C-string" $ do
+      assertEqual "" (Right ("abc", "def")) $
+        flip Pb.decodeByteString "abc\0def\0" $
+          (,) <$> Pb.byteStringTerminatedByNull <*> Pb.byteStringTerminatedByNull
   ]
