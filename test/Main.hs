3e5459f1f8b651a705f8c4ec285f6842103802f9
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-23 18:27:33 +0300

    Add a test on staticArray

--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import qualified Data.Serialize as Cereal
+import qualified Data.Vector.Unboxed as Vu
 import qualified PeekyBlinders as Pb
+import qualified Test.QuickCheck as Qc
 import Test.QuickCheck.Instances ()
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 import Prelude hiding (all, choose)
 
 main = defaultMain $ testGroup "All" all
@@ -19,5 +23,14 @@ all =
     testCase "Composition after C-string" $ do
       assertEqual "" (Right ("abc", "def")) $
         flip Pb.decodeByteString "abc\0def\0" $
-          (,) <$> Pb.nullTerminatedStringAsByteString <*> Pb.nullTerminatedStringAsByteString
+          (,) <$> Pb.nullTerminatedStringAsByteString <*> Pb.nullTerminatedStringAsByteString,
+    testProperty "staticArray" $ do
+      vec <- Qc.arbitrary @(Vu.Vector Int32)
+      let bs = Cereal.runPut $ do
+            Cereal.putInt32be $ fromIntegral $ Vu.length vec
+            Vu.forM_ vec $ Cereal.putInt32be
+          res = flip Pb.decodeByteString bs $ do
+            size <- Pb.statically Pb.beSignedInt4
+            Pb.statically $ Pb.staticArray Pb.beSignedInt4 $ fromIntegral size
+      return $ Right vec == res
   ]
