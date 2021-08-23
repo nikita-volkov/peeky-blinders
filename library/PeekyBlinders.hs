edfa1a8870ab805225a989dd7d59064f69538753
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-23 11:32:37 +0300

    Format with ormolu

--- library/PeekyBlinders.hs
+++ library/PeekyBlinders.hs
@@ -1,11 +1,13 @@
 module PeekyBlinders
   ( -- * Execution
     decodeByteString,
+
     -- * Dynamic
     Dynamic,
     dynamize,
     byteStringTerminatedByNull,
     shortByteStringTerminatedByNull,
+
     -- * Static
     Static,
     int32InBe,
@@ -15,35 +17,34 @@ module PeekyBlinders
   )
 where
 
-import PeekyBlinders.Prelude hiding (Dynamic)
-import qualified Data.ByteString.Internal as ByteString
 import qualified Data.ByteString.Char8 as Bsc
+import qualified Data.ByteString.Internal as ByteString
+import PeekyBlinders.Prelude hiding (Dynamic)
 import qualified Ptr.IO
 
 -- *
 
-{-|
-Execute a dynamic decoder on a bytestring,
-failing with the amount of extra bytes required at least if it\'s too short.
--}
+-- |
+-- Execute a dynamic decoder on a bytestring,
+-- failing with the amount of extra bytes required at least if it\'s too short.
 decodeByteString :: Dynamic a -> ByteString -> Either Int a
 decodeByteString (Dynamic peek) (ByteString.PS bsFp bsOff bsSize) =
-  unsafeDupablePerformIO $ withForeignPtr bsFp $ \p ->
-    peek (return . Left) (\r _ _ -> return (Right r)) (plusPtr p bsOff) bsSize
+  unsafeDupablePerformIO $
+    withForeignPtr bsFp $ \p ->
+      peek (return . Left) (\r _ _ -> return (Right r)) (plusPtr p bsOff) bsSize
 
 -- *
 
-{-|
-Instruction on how to decode a data-structure of size only known at runtime.
-
-Provides for monadic composition,
-where the output of one decoder determines what the following decoder should be.
-
-Not all encodings require that much compositional freedom and
-can be composed with a more restricted 'Static' decoder,
-which provides for higher performance at the cost of a more restrictive
-applicative composition.
--}
+-- |
+-- Instruction on how to decode a data-structure of size only known at runtime.
+--
+-- Provides for monadic composition,
+-- where the output of one decoder determines what the following decoder should be.
+--
+-- Not all encodings require that much compositional freedom and
+-- can be composed with a more restricted 'Static' decoder,
+-- which provides for higher performance at the cost of a more restrictive
+-- applicative composition.
 newtype Dynamic output = Dynamic (forall x. (Int -> IO x) -> (output -> Ptr Word8 -> Int -> IO x) -> Ptr Word8 -> Int -> IO x)
 
 instance Functor Dynamic where
@@ -61,10 +62,9 @@ instance Monad Dynamic where
 
 -- *
 
-{-|
-Convert a static decoder to the dynamic one.
-You can\'t go the other way around.
--}
+-- |
+-- Convert a static decoder to the dynamic one.
+-- You can\'t go the other way around.
 {-# INLINE dynamize #-}
 dynamize :: Static a -> Dynamic a
 dynamize (Static size io) = Dynamic $ \fail proceed p avail ->
@@ -72,43 +72,40 @@ dynamize (Static size io) = Dynamic $ \fail proceed p avail ->
     then io p >>= \x -> proceed x (plusPtr p size) (avail - size)
     else fail $ size - avail
 
-{-|
-C-style string, which is a collection of bytes terminated by the first 0-valued byte.
-This last byte is not included in the decoded value.
--}
+-- |
+-- C-style string, which is a collection of bytes terminated by the first 0-valued byte.
+-- This last byte is not included in the decoded value.
 byteStringTerminatedByNull :: Dynamic ByteString
 byteStringTerminatedByNull = Dynamic $ \fail proceed p avail -> do
   !bs <- Bsc.packCString (castPtr p)
   let sizeWithNull = succ (Bsc.length bs)
-    in if avail < sizeWithNull
-      then fail $ sizeWithNull - avail
-      else proceed bs (plusPtr p sizeWithNull) (avail - sizeWithNull)
+   in if avail < sizeWithNull
+        then fail $ sizeWithNull - avail
+        else proceed bs (plusPtr p sizeWithNull) (avail - sizeWithNull)
 
-{-|
-C-style string, which is a collection of bytes terminated by the first 0-valued byte.
-This last byte is not included in the decoded value.
--}
+-- |
+-- C-style string, which is a collection of bytes terminated by the first 0-valued byte.
+-- This last byte is not included in the decoded value.
 {-# INLINE shortByteStringTerminatedByNull #-}
 shortByteStringTerminatedByNull :: Dynamic ShortByteString
 shortByteStringTerminatedByNull = Dynamic $ \fail proceed p avail ->
   Ptr.IO.peekNullTerminatedShortByteString p $ \size build ->
     let sizeWithNull = succ size
-      in if avail < sizeWithNull
-        then fail $ sizeWithNull - avail
-        else build >>= \x -> proceed x (plusPtr p sizeWithNull) (avail - sizeWithNull)
+     in if avail < sizeWithNull
+          then fail $ sizeWithNull - avail
+          else build >>= \x -> proceed x (plusPtr p sizeWithNull) (avail - sizeWithNull)
 
 -- *
 
-{-|
-Instruction on how to decode a data-structure of a statically known size.
-
-Prefer composing on the level of this abstraction when possible,
-since it\'s faster.
-There is no way to lift a dynamic decoder to this level though,
-so you can only compose out of static decoders here.
--}
-data Static output =
-  Static {-# UNPACK #-} !Int (Ptr Word8 -> IO output)
+-- |
+-- Instruction on how to decode a data-structure of a statically known size.
+--
+-- Prefer composing on the level of this abstraction when possible,
+-- since it\'s faster.
+-- There is no way to lift a dynamic decoder to this level though,
+-- so you can only compose out of static decoders here.
+data Static output
+  = Static {-# UNPACK #-} !Int (Ptr Word8 -> IO output)
 
 instance Functor Static where
   fmap fn (Static size io) = Static size (fmap fn . io)
@@ -123,44 +120,40 @@ instance Applicative Static where
 
 -- *
 
-{-|
-4-byte signed Big-Endian integer.
--}
+-- |
+-- 4-byte signed Big-Endian integer.
 {-# INLINE int32InBe #-}
 int32InBe :: Static Int32
 int32InBe = Static 4 Ptr.IO.peekBEInt32
 
-{-|
-4-byte signed Little-Endian integer.
--}
+-- |
+-- 4-byte signed Little-Endian integer.
 {-# INLINE int32InLe #-}
 int32InLe :: Static Int32
 int32InLe = Static 4 Ptr.IO.peekLEInt32
 
-{-|
-Collect a strict bytestring knowing its size.
-
-Typically, you\'ll be using it like this:
-
-@
-byteString :: 'Dynamic' ByteString
-byteString = 'dynamize' 'int32InBe' >>= 'dynamize' . 'byteStringBySize' . fromIntegral
-@
--}
+-- |
+-- Collect a strict bytestring knowing its size.
+--
+-- Typically, you\'ll be using it like this:
+--
+-- @
+-- byteString :: 'Dynamic' ByteString
+-- byteString = 'dynamize' 'int32InBe' >>= 'dynamize' . 'byteStringBySize' . fromIntegral
+-- @
 {-# INLINE byteStringBySize #-}
 byteStringBySize :: Int -> Static ByteString
 byteStringBySize size = Static size $ \p -> Ptr.IO.peekBytes p size
 
-{-|
-Collect a short bytestring knowing its size.
-
-Typically, you\'ll be using it like this:
-
-@
-shortByteString :: 'Dynamic' ShortByteString
-shortByteString = 'dynamize' 'int32InBe' >>= 'dynamize' . 'shortByteStringBySize' . fromIntegral
-@
--}
+-- |
+-- Collect a short bytestring knowing its size.
+--
+-- Typically, you\'ll be using it like this:
+--
+-- @
+-- shortByteString :: 'Dynamic' ShortByteString
+-- shortByteString = 'dynamize' 'int32InBe' >>= 'dynamize' . 'shortByteStringBySize' . fromIntegral
+-- @
 {-# INLINE shortByteStringBySize #-}
 shortByteStringBySize :: Int -> Static ShortByteString
 shortByteStringBySize size = Static size $ \p -> Ptr.IO.peekShortByteString p size
--- test/Main.hs
+++ test/Main.hs
@@ -1,16 +1,17 @@
 module Main where
 
-import Prelude hiding (choose)
-import Test.Tasty
-import Test.Tasty.Runners
-import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
+import qualified Data.Serialize as Cereal
+import qualified PeekyBlinders as Pb
 import Test.QuickCheck
 import Test.QuickCheck.Instances
-import qualified PeekyBlinders
-import qualified Data.Serialize as Cereal
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+import Test.Tasty.Runners
+import Prelude hiding (all, choose)
 
+main = defaultMain $ testGroup "All" all
 
-main =
-  defaultMain $ testGroup "All" [
-    ]
+all =
+  [
+  ]
