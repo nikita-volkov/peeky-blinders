d2cec7843e658acd78c3433e24d8883ffdbbcca5
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-23 11:57:56 +0300

    Clean up the imports

--- test/Main.hs
+++ test/Main.hs
@@ -1,13 +1,9 @@
 module Main where
 
-import qualified Data.Serialize as Cereal
 import qualified PeekyBlinders as Pb
-import Test.QuickCheck
-import Test.QuickCheck.Instances
+import Test.QuickCheck.Instances ()
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Tasty.QuickCheck
-import Test.Tasty.Runners
 import Prelude hiding (all, choose)
 
 main = defaultMain $ testGroup "All" all
