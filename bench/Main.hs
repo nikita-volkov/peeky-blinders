408fd970997b47abb6c77a221950ba5aaf059f80
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-08-24 14:59:02 +0300

    Add array of byte arrays

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,6 +2,7 @@ import qualified Data.Persist as Persist
 import qualified Data.Serialize as Cereal
 import qualified Data.Store as Store
 import Data.String.ToString
+import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as Vu
 import Gauge.Main
 import qualified PeekyBlinders as Pb
@@ -31,6 +32,29 @@ main = do
            in either (const Nothing) Just . Cereal.runGet decoder
         )
       ]
+  arrayOfByteStringsBenchmarks <-
+    initArrayOfByteStringsGroup
+      [ ( "peeky-blinders",
+          let decoder = do
+                size <- Pb.statically Pb.leSignedInt8
+                Pb.dynamicArray @V.Vector byteStringDecoder $ fromIntegral size
+              byteStringDecoder = do
+                size <- Pb.statically Pb.leSignedInt8
+                Pb.statically $ Pb.byteArrayAsByteString $ fromIntegral size
+           in either (const Nothing) Just . Pb.decodeByteString decoder
+        ),
+        ( "store",
+          either (const Nothing) Just . Store.decode
+        ),
+        ( "cereal",
+          let decoder = do
+                size <- Cereal.getInt64le
+                V.replicateM (fromIntegral size) $ do
+                  size <- Cereal.getInt64le
+                  Cereal.getByteString $ fromIntegral size
+           in either (const Nothing) Just . Cereal.runGet decoder
+        )
+      ]
   putStrLn "Benchmarking"
   defaultMain
     [ bgroup "int32-le-triplet" $
@@ -42,7 +66,8 @@ main = do
               b "cereal" $ Cereal.runGet ((,,) <$> Cereal.getInt32le <*> Cereal.getInt32le <*> Cereal.getInt32le),
               b "persist" $ Persist.runGet ((,,) <$> Persist.getLE @Int32 <*> Persist.getLE @Int32 <*> Persist.getLE @Int32)
             ],
-      bgroup "array" arrayBenchmarks
+      bgroup "array-of-int4" arrayBenchmarks,
+      bgroup "array-of-byte-arrays" arrayOfByteStringsBenchmarks
     ]
 
 -- | Test functions and create benchmarks out of them.
@@ -54,6 +79,16 @@ initArrayGroup = initGroup input correctDecoding
         Cereal.putInt32le 100 <> replicateM_ 100 (Cereal.putInt32le (-1))
     correctDecoding =
       Vu.replicate 100 (-1)
+
+initArrayOfByteStringsGroup :: [(String, ByteString -> Maybe (V.Vector ByteString))] -> IO [Benchmark]
+initArrayOfByteStringsGroup = initGroup input correctDecoding
+  where
+    input = Cereal.runPut $ do
+      Cereal.putInt64le 100
+      replicateM_ 100 $ do
+        Cereal.putInt64le 3
+        Cereal.putByteString "abc"
+    correctDecoding = V.replicate 100 "abc"
 
 initGroup :: (Eq a, Show a, NFData a) => ByteString -> a -> [(String, ByteString -> Maybe a)] -> IO [Benchmark]
 initGroup input correctDecoding subjects = do
