21701707da770e2f9bf14021bbeb0596c123827e
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-25 18:23:17 +0300

    Clean up

--- library/PtrPeeker/FoldableExtras.hs
+++
@@ -1,8 +0,0 @@
-module PtrPeeker.FoldableExtras where
-
-import PtrPeeker.Prelude
-
-{-# INLINE consumeMonadically #-}
-consumeMonadically :: (Foldable f, Monad m) => f a -> b -> (b -> a -> m b) -> m b
-consumeMonadically foldable init step =
-  foldM step init foldable
